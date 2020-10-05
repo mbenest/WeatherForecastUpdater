243de9a4d502b0d9f3eb4ca126e5f800fb872dca
mbenest <matthieu1969@orange.fr>
2020-10-05 22:57:15 +0200

    Update compile_and_execute.sh

--- compile_and_execute.sh
+++ compile_and_execute.sh
@@ -2,7 +2,7 @@
 
 export JAVA_HOME="$1"
 
-jarfilename="WeatherForecastUpdater.jar"
+jarFilename="WeatherForecastUpdater.jar"
 binDirname="bin"
 
 find src -type f -name "*.java" > sources.txt
@@ -11,16 +11,16 @@ echo "Manifest-Version: 1.0" > MANIFEST.MF
 echo "Main-Class: eu.akka.weatherforecast.Launcher" >> MANIFEST.MF
 echo "Class-Path: ." >> MANIFEST.MF
 
-[ -e $jarfilename ] && rm $jarfilename
+[ -e $jarFilename ] && rm $jarFilename
 [ -d $binDirname ] && rm -rf $binDirname
 
 mkdir $binDirname
 
 $JAVA_HOME/bin/javac -d $binDirname @sources.txt
-$JAVA_HOME/bin/jar cvfm $jarfilename MANIFEST.MF -C $binDirname/ .
-$JAVA_HOME/bin/jar uf $jarfilename -C resources/ .
+$JAVA_HOME/bin/jar cvfm $jarFilename MANIFEST.MF -C $binDirname/ .
+$JAVA_HOME/bin/jar uf $jarFilename -C resources/ .
 
 rm MANIFEST.MF
 rm sources.txt
 
-$JAVA_HOME/bin/java -jar $jarfilename
+$JAVA_HOME/bin/java -jar $jarFilename
